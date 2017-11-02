aab1e25f6a38fada0b12aa95b2d383a80bc6e467
rubangru <33123244+rubangru@users.noreply.github.com>
2017-11-02 17:29:05 +0300

    Add files via upload

---
+++ TRPO_task0.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace TRPO_task0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader read = new StreamReader("text.txt");
+            string a;
+            a = read.ReadToEnd();
+            string b = @"(0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])[- /.][1-9][0-9][0-9][0-9]";
+            Regex regex = new Regex(b);
+            MatchCollection matches = regex.Matches(a);
+            StreamWriter write = new StreamWriter("text2.txt");
+            foreach (Match mat in matches)
+            {
+                write.Write(mat.Value + "  ");
+            }
+            write.Close();
+        }
+    }
+}
